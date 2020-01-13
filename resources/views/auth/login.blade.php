24247bdce432b3acf41ec6184d1a67c9f2c8dc7b
MediaEthic <emilie.rozis@erp-ethic.com>
2020-01-13 16:20:53 +0100

    :lipstick: Updating the UI and style of login file

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,164 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<div class="mx-auto h-full flex justify-center items-center bg-gray-300">
+    <div class="w-96 bg-blue-900 rounded-lg shadow-xl p-6">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+        <svg class="fill-current text-white w-16" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+             viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+<g>
+    <g>
+        <path d="M263.069,339.89c-1.859-1.86-4.439-2.93-7.069-2.93s-5.21,1.071-7.07,2.93c-1.86,1.86-2.93,4.44-2.93,7.07
+			c0,2.64,1.069,5.21,2.93,7.07c1.86,1.87,4.44,2.93,7.07,2.93s5.21-1.06,7.069-2.93c1.86-1.86,2.931-4.43,2.931-7.07
+			C266,344.33,264.93,341.76,263.069,339.89z"/>
+    </g>
+</g>
+            <g>
+                <g>
+                    <path d="M482,0.01H30c-16.542,0-30,13.432-30,29.943V362.98c0,16.511,13.458,29.943,30,29.943h176.979v59.261
+			c-25.227,2.555-44.98,23.917-44.98,49.806c0,5.523,4.478,10,10,10h168c5.522,0,10-4.477,10-10
+			c0-25.888-19.753-47.25-44.979-49.806v-59.261h13.313c5.522,0,10-4.477,10-10s-4.478-10-10-10H30c-5.514,0-10-4.46-10-9.943
+			v-41.948h306.9l-9.796,14.582c-2.061,3.067-2.262,7.02-0.524,10.28c1.738,3.26,5.131,5.297,8.825,5.297h25.916v64.038
+			c0,5.523,4.478,10,10,10h56.318c5.522,0,10-4.477,10-10v-22.306H482c16.542,0,30-13.432,30-29.943V29.953
+			C512,13.443,498.542,0.01,482,0.01z M226.98,394.01h58.041v57.917H226.98V394.01z M216.98,471.927h78.041h4.916
+			c13.073,0,24.222,8.386,28.352,20.063h-144.58c4.13-11.676,15.279-20.063,28.353-20.063H216.98z M417.638,331.191
+			c-5.522,0-10,4.477-10,10v64.038H371.32v-64.038c0-5.523-4.478-10-10-10h-17.151l9.773-14.547c0.01-0.015,0.02-0.03,0.03-0.045
+			l35.519-52.871l45.322,67.463H417.638z M492,362.98c0,5.483-4.486,9.943-10,9.943h-54.361v-21.732h25.939
+			c3.694,0,7.088-2.037,8.825-5.297s1.536-7.213-0.524-10.28l-9.796-14.582H492V362.98z M492,301.032h-53.353l-30.39-45.237H436
+			c16.542,0,30-13.458,30-30V95.178c0-16.542-13.458-30-30-30H289c-5.522,0-10,4.477-10,10s4.478,10,10,10h147
+			c5.514,0,10,4.486,10,10v130.617c0,5.514-4.486,10-10,10H76c-5.514,0-10-4.486-10-10V95.178c0-5.514,4.486-10,10-10h127.5
+			c5.522,0,10-4.477,10-10s-4.478-10-10-10H76c-16.542,0-30,13.458-30,30v130.617c0,16.542,13.458,30,30,30h294.726l-30.39,45.237
+			H20V29.953c0-5.482,4.486-9.943,10-9.943h452c5.514,0,10,4.46,10,9.943V301.032z"/>
+                </g>
+            </g>
+            <g>
+                <g>
+                    <path d="M253.479,68.11c-1.859-1.86-4.439-2.93-7.069-2.93s-5.21,1.07-7.07,2.93c-1.86,1.86-2.93,4.44-2.93,7.07
+			s1.069,5.21,2.93,7.07c1.861,1.86,4.44,2.93,7.07,2.93s5.21-1.07,7.069-2.93c1.86-1.86,2.931-4.44,2.931-7.07
+			S255.34,69.97,253.479,68.11z"/>
+                </g>
+            </g>
+            <g>
+                <g>
+                    <path d="M214.876,190.058l-17.782-17.781c11.706-2.948,20.399-13.56,20.399-26.167c0-14.877-12.104-26.981-26.981-26.981h-16.745
+			c-5.522,0-10,4.477-10,10v33.958c0,0.003,0,0.007,0,0.01v33.957c0,5.523,4.478,10,10,10c5.522,0,10-4.477,10-10v-9.82
+			l16.967,16.966c1.953,1.953,4.512,2.929,7.071,2.929s5.118-0.977,7.071-2.929C218.781,200.295,218.781,193.963,214.876,190.058z
+			 M190.512,153.091h-6.745v-13.963h6.745c3.85,0,6.981,3.132,6.981,6.981S194.361,153.091,190.512,153.091z"/>
+                </g>
+            </g>
+            <g>
+                <g>
+                    <path d="M338.649,119.127c-5.522,0-10,4.477-10,10v38.547l-34.38-44.648c-2.611-3.392-7.09-4.745-11.146-3.366
+			c-4.053,1.38-6.777,5.186-6.777,9.467v67.925c0,5.524,4.477,10.001,10,10.001c5.522,0,10-4.477,10-10v-38.547l34.38,44.648
+			c1.93,2.506,4.88,3.899,7.925,3.899c1.075,0,2.162-0.174,3.221-0.534c4.053-1.38,6.777-5.186,6.777-9.467v-67.925
+			C348.649,123.604,344.171,119.127,338.649,119.127z"/>
+                </g>
+            </g>
+            <g>
+                <g>
+                    <path d="M122.272,119.128h-16.745c-5.522,0-10,4.477-10,10v33.963v34.037c0,5.523,4.478,10,10,10c5.522,0,10-4.477,10-10v-24.037
+			h6.745c14.878,0,26.981-12.104,26.981-26.981S137.15,119.128,122.272,119.128z M122.272,153.091h-6.745v-13.963h6.745
+			c3.85,0,6.981,3.132,6.981,6.981S126.122,153.091,122.272,153.091z"/>
+                </g>
+            </g>
+            <g>
+                <g>
+                    <path d="M246.409,120.093c-5.522,0-10,4.477-10,10v65.996c0,5.523,4.478,10,10,10c5.522,0,10-4.477,10-10v-65.996
+			C256.409,124.57,251.931,120.093,246.409,120.093z"/>
+                </g>
+            </g>
+            <g>
+                <g>
+                    <path d="M406.472,119.128H372.51c-5.522,0-10,4.477-10,10c0,5.523,4.477,10,10,10h6.981v56.96c0,5.523,4.478,10,10,10
+			c5.522,0,10-4.477,10-10v-56.96h6.981c5.522,0,10-4.477,10-10C416.472,123.605,411.994,119.128,406.472,119.128z"/>
+                </g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+</svg>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+        <h1 class="text-white text-3xl pt-8">Bon retour parmi nous sur Printingbot</h1>
+        <h2 class="text-blue-300">Saisissez vos identifiants</h2>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+        <form method="POST" action="{{ route('login') }}" class="pt-8">
+            @csrf
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+            <div class="relative">
+                <label for="email" class="uppercase text-blue-500 text-xs font-bold absolute pl-3 pt-2">{{ __('Email address') }}</label>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                <div>
+                    <input id="email" type="email" class="pt-8 w-full rounded p-3 bg-blue-800 text-gray-100 outline-none focus:bg-blue-700" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="your@email.com">
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    @error('email')
+                    <span class="text-red-600 text-sm pt-1" role="alert"><strong>{{ $message }}</strong></span>
+                    @enderror
                 </div>
             </div>
-        </div>
+
+            <div class="relative pt-3">
+                <label for="password" class="pt-8 w-full rounded p-3 bg-blue-800 text-gray-100 outline-none focus:bg-blue-700">{{ __('Password') }}</label>
+
+                <div>
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="password">
+
+                    @error('password')
+                    <span class="text-red-600 text-sm pt-1" role="alert"><strong>{{ $message }}</strong></span>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="pt-3">
+                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                <label class="text-white" for="remember">
+                    {{ __('Remember Me') }}
+                </label>
+            </div>
+
+            <div class="pt-8">
+                <button type="submit"
+                        class="w-full bg-gray-400 py-2 px-3 text-left uppercase rounded text-blue-800 font-bold">
+                    {{ __('Login') }}
+                </button>
+            </div>
+
+            <div class="flex justify-between pt-8 text-white text-sm font-bold">
+                <a class="btn btn-link" href="{{ route('password.request') }}">
+                    {{ __('Forgot Your Password?') }}
+                </a>
+                <a class="btn btn-link" href="{{ route('register') }}">
+                    {{ __('Register') }}
+                </a>
+            </div>
+        </form>
     </div>
 </div>
 @endsection
