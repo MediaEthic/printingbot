d2edc4894678420913a471a058fa71889607f60f
MediaEthic <emilie.rozis@erp-ethic.com>
2020-01-15 15:27:22 +0100

    :lipstick: Updating style files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,43 @@
 module.exports = {
-  theme: {
-    extend: {
-        width: {
-            '96': '24rem'
+    theme: {
+        colors: {
+            purple1: '#672767',
+            purple3: '#A57AA0',
+            purple4: '#C3A5BF',
+            purple5: '#E2D1DE',
+            yellow1: '#FFCE00',
+            red1: '#E8004C',
+            red5: '#FFD7D7',
+            black: '#3D1152',
+            grey: '#E6E2E7',
+            white: '#FFFFFF',
+        },
+        fontFamily: {
+            'sans': ['"Source Sans Pro"', 'Helvetica', 'Arial', 'sans-serif'],
+            'serif': ['"Source Serif Pro"', 'Georgia', 'Cambria', 'serig'],
+        },
+        fontSize: {
+            'xs': '1rem',
+            'sm': '1.125rem',
+            'tiny': '1.25rem',
+            'base': '1.5rem',
+            'lg': '1.875rem',
+            'xl': '2.25rem',
+            '2xl': '3rem',
+            '3xl': '4rem',
+            '4xl': '5rem',
+            '5xl': '6rem',
+            '6xl': '7rem',
+            '7xl': '8rem',
+        },
+        extend: {
+            width: {
+                '76': '19rem',
+                '100': '25rem',
+                '120': '30rem'
+            }
         }
-    }
-  },
-  variants: {},
-  plugins: []
+    },
+    variants: {},
+    plugins: []
 }
