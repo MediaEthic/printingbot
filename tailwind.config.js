379ca13be57626891d3e71cdb6fe4c3412aede01
MediaEthic <emilie.rozis@erp-ethic.com>
2020-01-24 14:38:30 +0100

    :wrench: Adding configuration files for dompdf and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,5 @@
 module.exports = {
     theme: {
-        colors: {
-            purple1: '#672767',
-            purple3: '#A57AA0',
-            purple4: '#C3A5BF',
-            purple5: '#E2D1DE',
-            yellow1: '#FFCE00',
-            red1: '#E8004C',
-            red5: '#FFD7D7',
-            black: '#3D1152',
-            grey: '#E6E2E7',
-            white: '#FFFFFF',
-        },
         fontFamily: {
             'sans': ['"Source Sans Pro"', 'Helvetica', 'Arial', 'sans-serif'],
             'serif': ['"Source Serif Pro"', 'Georgia', 'Cambria', 'serig'],
@@ -23,18 +11,52 @@ module.exports = {
             'base': '1.5rem',
             'lg': '1.875rem',
             'xl': '2.25rem',
-            '2xl': '3rem',
-            '3xl': '4rem',
-            '4xl': '5rem',
-            '5xl': '6rem',
-            '6xl': '7rem',
-            '7xl': '8rem',
+            '2xl': '2.5rem',
+            '3xl': '3rem',
+            '4xl': '4rem',
+            '5xl': '5rem',
+            '6xl': '6rem',
+            '7xl': '7rem',
         },
         extend: {
             width: {
                 '76': '19rem',
                 '100': '25rem',
                 '120': '30rem'
+            },
+            colors: {
+                purple1: '#672767',
+                purple2: '#865083',
+                purple3: '#A57AA0',
+                purple4: '#C3A5BF',
+                purple5: '#E2D1DE',
+                yellow1: '#FFCE00',
+                yellow5: '#FFF5D3',
+                red1: '#E8004C',
+                red5: '#FFD7D7',
+                black: '#3D1152',
+                grey: '#E6E2E7',
+                white: '#FFFFFF',
+            },
+            borderRadius: {
+                'xl': '1rem',
+                '2xl': '2rem',
+                '3xl': '3rem',
+                '4xl': '4rem',
+            },
+            minWidth: {
+                '100': '25rem',
+                '120': '30rem',
+            },
+            maxWidth: {
+                'initial': 'initial',
+                '100': '25rem',
+                '120': '30rem',
+            },
+            inset: {
+                '7': '1.75rem',
+                '12': '3rem',
+                '20': '5rem',
             }
         }
     },
