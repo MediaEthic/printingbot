416a90c753144cc987c08cae85927d10f40d5740
MediaEthic <emilie.rozis@erp-ethic.com>
2020-01-29 14:14:57 +0100

    :card_file_box: Adding thirds migration and model files

---
+++ app/Models/Third.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Third extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'updated_by',
+        'type',
+        'name',
+        'alias',
+        'logo',
+        'category_id',
+        'order_confirmation',
+        'bank_id',
+        'parent_id',
+        'customer',
+        'address_line1',
+        'address_line2',
+        'address_line3',
+        'zipcode',
+        'city',
+        'country_id',
+        'language_id',
+        'time_zone_id',
+        'email',
+        'phone',
+        'ape',
+        'siret',
+        'intracommunity_no',
+        'territory_id',
+        'user_id',
+        'commission_rate',
+        'discount_rate',
+        'bank_rate',
+        'discount_duration',
+        'payment_id',
+        'settlement_id',
+        'vat_id',
+        'holder',
+        'bank',
+        'bank_code',
+        'sort_code',
+        'account',
+        'rib',
+        'iban',
+        'bic',
+        'subledger_account',
+        'concentration_account',
+        'invoice_number',
+        'invoice_copy',
+        'monthend_invoices',
+        'grouping_invoices',
+        'statement',
+        'credit_control',
+        'credit_limit',
+        'credit_insurance',
+        'allow_order',
+        'head_order',
+        'foot_order',
+        'head_delivery',
+        'foot_delivery',
+        'head_invoice',
+        'foot_invoice',
+        'technical_remarks',
+        'accounting_remarks',
+        'mention_id',
+        'active',
+    ];
+
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'thirds';
+
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+
+    /**
+     * Get the user of the third.
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User', 'updated_by', 'id');
+    }
+
+    /**
+     * Get the country of the third.
+     */
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    /**
+     * Get the language of the third.
+     */
+    public function language()
+    {
+        return $this->belongsTo(Language::class);
+    }
+
+    /**
+     * Get the timezone of the third.
+     */
+    public function timezone()
+    {
+        return $this->belongsTo(TimeZone::class);
+    }
+
+    /**
+     * Get the salesperson of the third.
+     */
+    public function salesperson()
+    {
+        return $this->belongsTo('App\Models\User', 'user_id', 'id');
+    }
+
+    /**
+     * Get the payment of the third.
+     */
+    public function payment()
+    {
+        return $this->belongsTo(Payment::class);
+    }
+
+    /**
+     * Get the settlement of the third.
+     */
+    public function settlement()
+    {
+        return $this->belongsTo(Settlement::class);
+    }
+
+    /**
+     * Get the vat of the third.
+     */
+    public function vat()
+    {
+        return $this->belongsTo(Vat::class);
+    }
+
+
+    /**
+     * Get all of the invoices for the third.
+     */
+    public function invoices()
+    {
+        return $this->hasMany(Invoice::class);
+    }
+}
---
+++ database/migrations/2020_01_27_141732_create_thirds_table.php
@@ -0,0 +1,153 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateThirdsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('thirds', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->nullableTimestamps();
+            $table->unsignedBigInteger('updated_by')->nullable()->default(null);
+            $table->foreign('updated_by')
+                ->references('id')
+                ->on('users')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+            $table->enum('type', ['supplier', 'prospect', 'customer'])->default('customer');
+            $table->string('name', 38);
+            $table->string('alias', 10)->nullable()->default(null);
+            $table->string('logo')->nullable()->default(null);
+            $table->unsignedBigInteger('category_id')->nullable()->default(null);
+            $table->enum('order_confirmation', ['Email', 'Post', 'Fax'])->default('Email');
+            $table->unsignedBigInteger('bank_id')->nullable()->default(null);
+            $table->unsignedBigInteger('parent_id')->nullable()->default(null);
+            $table->foreign('parent_id')
+                ->references('id')
+                ->on('thirds')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+            $table->date('customer')->nullable()->default(null);
+
+            $table->string('address_line1')->nullable()->default(null);
+            $table->string('address_line2')->nullable()->default(null);
+            $table->string('address_line3')->nullable()->default(null);
+            $table->string('zipcode', 10)->nullable()->default(null);
+            $table->string('city', 38)->nullable()->default(null);
+            $table->unsignedInteger('country_id')->nullable()->default(75);
+//            $table->foreign('country_id')
+//                ->references('id')
+//                ->on('countries')
+//                ->onDelete('restrict')
+//                ->onUpdate('restrict');
+            $table->unsignedInteger('language_id')->nullable()->default(47);
+//            $table->foreign('language_id')
+//                ->references('id')
+//                ->on('languages')
+//                ->onDelete('restrict')
+//                ->onUpdate('restrict');
+            $table->unsignedInteger('time_zone_id')->nullable()->default(40);
+//            $table->foreign('time_zone_id')
+//                ->references('id')
+//                ->on('time_zones')
+//                ->onDelete('restrict')
+//                ->onUpdate('restrict');
+            $table->string('email')->nullable()->default(null);
+            $table->string('phone')->nullable()->default(null);
+
+            $table->string('ape', 5)->nullable()->default(null);
+            $table->string('siret', 17)->nullable()->default(null);
+            $table->string('intracommunity_no', 20)->nullable()->default(null);
+            $table->unsignedBigInteger('territory_id')->nullable()->default(null);
+            $table->unsignedBigInteger('user_id')->nullable()->default(null);
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+            $table->double('commission_rate')->nullable()->default(null);
+            $table->double('discount_rate')->nullable()->default(null);
+            $table->double('bank_rate')->nullable()->default(null);
+            $table->unsignedInteger('discount_duration')->nullable()->default(null);
+            $table->unsignedBigInteger('payment_id')->nullable()->default(1);
+            $table->foreign('payment_id')
+                ->references('id')
+                ->on('payments')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+            $table->unsignedBigInteger('settlement_id')->nullable()->default(1);
+            $table->foreign('settlement_id')
+                ->references('id')
+                ->on('settlements')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+            $table->unsignedBigInteger('vat_id')->nullable()->default(1);
+            $table->foreign('vat_id')
+                ->references('id')
+                ->on('vats')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+
+            $table->string('holder', 80)->nullable()->default(null);
+            $table->string('bank', 80)->nullable()->default(null);
+            $table->string('bank_code', 5)->nullable()->default(null);
+            $table->string('sort_code', 5)->nullable()->default(null);
+            $table->string('account', 11)->nullable()->default(null);
+            $table->string('rib', 2)->nullable()->default(null);
+            $table->string('iban', 35)->nullable()->default(null);
+            $table->string('bic', 15)->nullable()->default(null);
+            $table->string('subledger_account', 20)->nullable()->default(null);
+            $table->unsignedInteger('concentration_account')->nullable()->default(null);
+            $table->unsignedTinyInteger('invoice_number')->default('1');
+            $table->unsignedTinyInteger('invoice_copy')->default('0');
+            $table->boolean('monthend_invoices')->default(false);
+            $table->boolean('grouping_invoices')->default(true);
+            $table->boolean('statement')->default(false);
+            $table->enum('credit_control', ['none', 'report', 'block', 'forbid'])->default('none');
+            $table->unsignedInteger('credit_limit')->nullable()->default(null);
+            $table->unsignedInteger('credit_insurance')->nullable()->default(null);
+            $table->boolean('allow_order')->default(false);
+
+            $table->text('head_order')->nullable()->default(null);
+            $table->text('foot_order')->nullable()->default(null);
+            $table->text('head_delivery')->nullable()->default(null);
+            $table->text('foot_delivery')->nullable()->default(null);
+            $table->text('head_invoice')->nullable()->default(null);
+            $table->text('foot_invoice')->nullable()->default(null);
+            $table->text('technical_remarks')->nullable()->default(null);
+            $table->text('accounting_remarks')->nullable()->default(null);
+            $table->unsignedBigInteger('mention_id')->nullable()->default(null);
+            $table->boolean('active')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('thirds', function (Blueprint $table) {
+            $table->dropForeign(['updated_by']);
+            $table->dropForeign(['parent_id']);
+//            $table->dropForeign(['country_id']);
+//            $table->dropForeign(['language_id']);
+//            $table->dropForeign(['time_zone_id']);
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['payment_id']);
+            $table->dropForeign(['settlement_id']);
+            $table->dropForeign(['vat_id']);
+        });
+
+        Schema::dropIfExists('thirds');
+    }
+}
